b416c947558dd0310aa47425a9322f36a0e7fdce
alaminislamrahat <alaminislamrahat@gmail.com>
2025-03-11 01:17:29 +0600

    insert many data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const todoHandler = require('./routHandler/todoHandler');
 
 
 const app = express();
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const port = 5000;
 
-const uri = "mongodb+srv://mongooseUser:XX2Qw2Bu0hncOmZk@cluster0.3jkraio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://mongooseUser:Afyk9jaNAPAlN9rJ@cluster0.3jkraio.mongodb.net/TodoDb";
 
 mongoose.connect(uri)
     .then(() => console.log('connection success fully'))
--- routHandler/todoHandler.js
+++ routHandler/todoHandler.js
@@ -2,30 +2,52 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const todoSchema = require('../schemas/todoSchema')
-const Todo = new mongoose.Model("Todo",todoSchema);
+const Todo = new mongoose.model("Todo", todoSchema);
 
 //get all todos
-router.get('/', async(req, res) => {
-    
+router.get('/', async (req, res) => {
+
 })
 //get a todo
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
 
 })
 //post a todo
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
+    try {
+        const newTodo = new Todo(req.body);
+        await newTodo.save(); // No callback needed
 
-})
+        res.status(200).json({
+            message: "Todo was inserted successfully"
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: "There was a server-side error"
+        });
+    }
+});
 //post multiple todos
-router.post('/all', async(req, res) => {
-
+router.post('/all', async (req, res) => {
+    try {
+        await Todo.insertMany(req.body)
+        res.status(200).json({
+            message: "Todo was inserted successfully"
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            error: "There was a server-side error"
+        });
+    }
 })
 //put  todo
-router.put('/:id', async(req, res) => {
+router.put('/:id', async (req, res) => {
+
 
 })
 //delete a todo
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', async (req, res) => {
 
 })
 
